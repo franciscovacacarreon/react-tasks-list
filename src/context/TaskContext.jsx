cd686e4440fa3de045a85c8c47dddd64569fe414
Francisco Vaca Carreon <vacafrancisco8@gmail.com>
2024-03-24 01:49:53 -0400

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/tasks";
+
+// Nombre del contexto
+export const TaskContext = createContext();
+
+// Componente que engloba a todos
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
